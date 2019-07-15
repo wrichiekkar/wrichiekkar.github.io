5055189a6a6070bed689c27f160d3d1bfe35cfc7
wrichiekkar <wrichiek.kar@ryerson.ca>
2019-07-15 15:32:00 -0400

    update

--- TheSourceBusinessCase/index.js
+++ TheSourceBusinessCase/index.js
@@ -1,4 +1,4 @@
-function login() {
+function Login() {
     if (firebase.auth().currentUser) {
       // [START signout]
       firebase.auth().signOut();
@@ -33,14 +33,13 @@ function login() {
       // [END authwithemail]
     }
     document.getElementById('quickstart-sign-in').disabled = true;
-}
+  }
   /**
    * Handles the sign up button press.
    */
-function handleSignUp() {
+  function SignUp() {
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
-
     if (email.length < 4) {
       alert('Please enter an email address.');
       return;
@@ -69,7 +68,7 @@ function handleSignUp() {
   /**
    * Sends an email verification to the user.
    */
-function sendEmailVerification() {
+  function sendEmailVerification() {
     // [START sendemailverification]
     firebase.auth().currentUser.sendEmailVerification().then(function() {
       // Email Verification sent!
@@ -79,7 +78,7 @@ function sendEmailVerification() {
     });
     // [END sendemailverification]
   }
-function sendPasswordReset() {
+  function sendPasswordReset() {
     var email = document.getElementById('email').value;
     // [START sendpasswordemail]
     firebase.auth().sendPasswordResetEmail(email).then(function() {
@@ -107,48 +106,3 @@ function sendPasswordReset() {
    *  - firebase.auth().onAuthStateChanged: This listener is called when the user is signed in or
    *    out, and that is where we update the UI.
    */
-function initApp() {
-    // Listening for auth state changes.
-    // [START authstatelistener]
-    firebase.auth().onAuthStateChanged(function(user) {
-      // [START_EXCLUDE silent]
-      document.getElementById('quickstart-verify-email').disabled = true;
-      // [END_EXCLUDE]
-      if (user) {
-        // User is signed in.
-        var displayName = user.displayName;
-        var email = user.email;
-        var emailVerified = user.emailVerified;
-        var photoURL = user.photoURL;
-        var isAnonymous = user.isAnonymous;
-        var uid = user.uid;
-        var providerData = user.providerData;
-        // [START_EXCLUDE]
-        document.getElementById('quickstart-sign-in-status').textContent = 'Signed in';
-        document.getElementById('quickstart-sign-in').textContent = 'Sign out';
-        document.getElementById('quickstart-account-details').textContent = JSON.stringify(user, null, '  ');
-        if (!emailVerified) {
-          document.getElementById('quickstart-verify-email').disabled = false;
-        }
-        // [END_EXCLUDE]
-      } else {
-        // User is signed out.
-        // [START_EXCLUDE]
-        document.getElementById('quickstart-sign-in-status').textContent = 'Signed out';
-        document.getElementById('quickstart-sign-in').textContent = 'Sign in';
-        document.getElementById('quickstart-account-details').textContent = 'null';
-        // [END_EXCLUDE]
-      }
-      // [START_EXCLUDE silent]
-      document.getElementById('quickstart-sign-in').disabled = false;
-      // [END_EXCLUDE]
-    });
-    // [END authstatelistener]
-    document.getElementById('quickstart-sign-in').addEventListener('click', toggleSignIn, false);
-    document.getElementById('quickstart-sign-up').addEventListener('click', handleSignUp, false);
-    document.getElementById('quickstart-verify-email').addEventListener('click', sendEmailVerification, false);
-    document.getElementById('quickstart-password-reset').addEventListener('click', sendPasswordReset, false);
-  }
-  window.onload = function() {
-    initApp();
-  };
