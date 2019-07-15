7b615ec5aed6647a885e8f3400517d1389290240
wrichiekkar <wrichiek.kar@ryerson.ca>
2019-07-15 14:00:42 -0400

    Update index.js

--- TheSourceBusinessCase/index.js
+++ TheSourceBusinessCase/index.js
@@ -12,7 +12,7 @@ function login(){
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
 
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
