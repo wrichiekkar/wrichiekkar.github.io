29dabeaa78cea5c4f3dfde6df6695d8ec8f94470
wrichiekkar <wrichiek.kar@ryerson.ca>
2019-07-15 16:50:38 -0400

    update

---
+++ TheSourceBusinessCase/login.js
@@ -0,0 +1,27 @@
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+
+      document.getElementById("MemberPage").style.display="block";
+      document.getElementById("login").style.display="none";
+    } else {
+      // No user is signed in.
+      document.getElementById("MemberPage").style.display="none";
+      document.getElementById("login").style.display="block";
+    }
+  });
+
+function login(){
+    
+    var userEmail = document.getElementById("email_field").value;
+    var userPass = document.getElementById("password_field").value;
+
+    firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        
+        window.alert("Error : " + errorMessage);
+      });
+}
+
