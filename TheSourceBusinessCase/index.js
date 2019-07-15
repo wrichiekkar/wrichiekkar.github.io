7340f97b1b3dabf378c7c9f3a00389bc9d7de07f
wrichiekkar <wrichiek.kar@ryerson.ca>
2019-07-15 13:06:39 -0400

    update

--- TheSourceBusinessCase/index.js
+++ TheSourceBusinessCase/index.js
@@ -1,3 +1,18 @@
-function(){
-    window.alert("Working");
+function login(){
+    
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+          window.location.href="http://wrichiekkar.me/MemberPage"
+        } else {
+          // No user is signed in.
+        }
+      });
+
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+
+    window.alert(email + password);
+
+
+
 }
