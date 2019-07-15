5dace041b5f68129498c31dfdf5ee3fe5c1ffae6
wrichiekkar <wrichiek.kar@ryerson.ca>
2019-07-15 13:16:29 -0400

    update

--- TheSourceBusinessCase/index.js
+++ TheSourceBusinessCase/index.js
@@ -1,18 +1,21 @@
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      window.location.href="http://wrichiekkar.me/TheSourceBusinessCase/MemberPage"
+    } else {
+    }
+  });
+
 function login(){
-    
-    firebase.auth().onAuthStateChanged(function(user) {
-        if (user) {
-          window.location.href="http://wrichiekkar.me/MemberPage"
-        } else {
-          // No user is signed in.
-        }
-      });
 
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
 
-    window.alert(email + password);
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        
+        window.alert("Incorrect login, try again")
 
-
-
+      });
 }
